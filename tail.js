b06c1f6b61d11520d99ebd2b057a373b6102baf9
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-01 23:13:37 -0700

    slice usage

---
+++ tail.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const tail = function(array) {
+  return array.slice(1);
+};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
