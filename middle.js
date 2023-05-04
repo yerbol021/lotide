028677e620d1225489bb9009e179f126f5b25df2
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-03 17:33:23 -0700

    middle added

---
+++ middle.js
@@ -0,0 +1,41 @@
+function middle(arr) {
+  if (arr.length < 3) {
+    return [];
+  }
+  let position = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 1) {
+    return [arr[position]];
+  }
+  else {
+    return [arr[position - 1], arr[position]];
+  }
+}
+ 
+// Define assertEqual function
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
