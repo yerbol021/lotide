2b7cd06e89c26f63e63d33bf0d837dc8d27938ed
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-04 17:58:56 -0700

    a=a

---
+++ map.js
@@ -0,0 +1,35 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+// Define assertEqual function
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Test
+assertArraysEqual(map(['apple', 'banana'], word => word[0]), ['a', 'b']);
