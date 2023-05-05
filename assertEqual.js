de69769298b57cfe2ba8094cbfe1b35394668d84
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-05 16:34:53 -0700

    rewrited the func

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,13 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+    return;
+  } 
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+// check for error
