35dd5ba99c57aeed33a5ab8f15b538b59456c206
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-08 22:28:35 -0700

    M & C

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,18 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
 
-// TEST CODE
-assertEqual(head([4, 5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// // TEST CODE
+// assertEqual(head([4, 5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 4 for [4, 5, 6, 7]", () => {
+    assert.strictEqual(head([4, 5, 6, 7]), 4);
+  });
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
