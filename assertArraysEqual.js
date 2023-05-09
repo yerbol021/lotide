511a25378bea82949fa5daaa8f1498224a0ea753
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-08 21:16:32 -0700

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,16 @@
 // Define assertEqual function
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+const eqArrays = require ('./eqArrays');
 
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -20,8 +21,9 @@ const assertArraysEqual = function (actual, expected) {
 };
 
 
-// Test
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 3]), false); // => should PASS
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+// // Test
+// assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 3]), false); // => should PASS
+// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+// assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+module.exports = assertArraysEqual; 
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require ('../assertArraysEqual');
+const eqArrays = require ('../eqArrays');
+// Test
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 3]), false); // => should PASS
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
