08dc642ab49211bd4c37da065b46bdb5e3d933ab
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-05-14 23:48:26 -0700

    q

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+// Test the function
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual(result1, "noma");  // should print: ✅ Assertion Passed: noma === noma
+
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli
