2449b08513247f1ba96e143b52cfa53e2b61de87
yerbol021 <125719656+yerbol021@users.noreply.github.com>
2023-08-22 23:54:13 -0700

    Update takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -37,3 +37,5 @@ assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+//revised
